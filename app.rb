a8eae23c946344f216637b70b7f42bba65d446ce
Erin <eteague27@gmail.com>
2015-12-23 17:34:33 -0500

    user structure done, adding post feature. need to debug displaying posts on index

--- app.rb
+++ app.rb
@@ -5,6 +5,62 @@ require "./models"
 
 set :database, "sqlite3:erinsblogdb.sqlite3"
 
+enable :sessions
+
 get '/' do
+	@user = current_user
 	erb :'sign-in'	
 end
+
+get '/sign-in' do 
+	erb :'sign-in'
+end
+
+get '/index' do
+	@user = current_user
+	@post = Post.all
+	erb :index
+end
+
+get '/sign-up' do
+	erb :'sign-up'
+end
+
+post '/sign-in' do
+	@user = User.where(email: params[:email]).last
+	if @user && @user.password == params[:password]
+		session[:user_id] = @user.id
+		redirect '/index'
+	else
+		flash[:alert] = "Your email or password is incorrect."
+		redirect '/sign-in'
+	end
+end
+
+post '/sign-up' do
+	@user = User.create(fname: params[:fname], lname: params[:lname], email: params[:email], password: params[:password])
+	redirect '/sign-in'
+end
+
+post '/new-post' do
+	@posts = Post.all
+	erb :'new-post'
+	redirect :index
+end
+
+get '/new-post' do
+	@posts = Post.new(params[:post])
+	erb :'new-post'
+end
+
+get '/logout' do
+	session.clear
+	redirect '/sign-in'
+end
+
+def current_user
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+	end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223200221) do
+ActiveRecord::Schema.define(version: 20151223214710) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "fname"
